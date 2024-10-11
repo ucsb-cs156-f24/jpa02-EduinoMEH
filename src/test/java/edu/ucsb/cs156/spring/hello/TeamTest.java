96d996d66a408230ee81a588ca79bb67443beb9f
Edwin Medrano <eduino@169-231-173-113.wireless.ucsb.edu>
2024-10-11 13:24:10 -0700

    EMV - killed all mutations and updated README

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,6 +2,9 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,8 +22,109 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_returns_true_for_same_objects() {
+        Team team1 = new Team();
+        Team team2 = team1;
+        assertEquals(team1.equals(team2), true);
+    }
+
+    @Test
+    public void equals_returns_false_for_different_classes() {
+        Team team1 = new Team();
+        Object obj = new Object();
+        assertEquals(team1.equals(obj), false);
+    }
+
+    // Multiple tests for (this.name.equals(other.name) && this.members.equals(other.members))
+    @Test
+    public void equals_returns_true_for_same_name_and_members() {
+        Team team1 = new Team();
+        Team team2 = new Team();
+        ArrayList<String> memberList = new ArrayList<String>();
+        memberList.add("Edwin");
+
+        team1.setName("f24-02");
+        team1.setMembers(memberList);
+        team2.setName("f24-02");
+        team2.setMembers(memberList);
+
+        assertEquals(team1.equals(team2), true);
+    }
+
+    @Test
+    public void equals_return_false_for_same_name_different_members() {
+        Team team1 = new Team();
+        Team team2 = new Team();
+        ArrayList<String> memberList1 = new ArrayList<String>();
+        ArrayList<String> memberList2 = new ArrayList<String>();
+        memberList1.add("Edwin");
+        memberList2.add("Jakob");
+
+        team1.setName("f24-02");
+        team1.setMembers(memberList1);
+        team2.setName("f24-02");
+        team2.setMembers(memberList2);
+
+        assertEquals(team1.equals(team2), false);
+    }
+
+    @Test
+    public void equals_return_false_for_different_name_same_members() {
+        Team team1 = new Team();
+        Team team2 = new Team();
+        ArrayList<String> memberList = new ArrayList<String>();
+        memberList.add("Edwin");
+
+        team1.setName("f24-02");
+        team1.setMembers(memberList);
+        team2.setName("f24-03");
+        team2.setMembers(memberList);
+
+        assertEquals(team1.equals(team2), false);
+    }
+
+    @Test
+    public void equals_return_false_for_different_name_and_members() {
+        Team team1 = new Team();
+        Team team2 = new Team();
+        ArrayList<String> memberList1 = new ArrayList<String>();
+        ArrayList<String> memberList2 = new ArrayList<String>();
+        memberList1.add("Edwin");
+        memberList2.add("Jakob");
+
+        team1.setName("f24-02");
+        team1.setMembers(memberList1);
+        team2.setName("f24-03");
+        team2.setMembers(memberList2);
+
+        assertEquals(team1.equals(team2), false);
+    }
+  
+    @Test
+    public void hashCode_returns_true_same_class() {
+        Team team1 = new Team();
+        Team team2 = new Team();
+        ArrayList<String> memberList = new ArrayList<String>();
+        memberList.add("Edwin");
+
+        team1.setName("f24-02");
+        team1.setMembers(memberList);
+        team2.setName("f24-02");
+        team2.setMembers(memberList);
+        assertEquals(team1.hashCode(), team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_distinction_OR_AND() {
+        Team team1 = new Team();
+        int result = team1.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 }
